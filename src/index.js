5b85bb1ddc38c86c2da514283d114920d6a90741
judithrauland <97590487+judithrauland@users.noreply.github.com>
2022-01-18 20:54:24 +0100

    Added sunrise and sunset time

--- src/index.js
+++ src/index.js
@@ -24,6 +24,21 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+// Time for sunrise and sunset
+function formatSun(timestamp) {
+  let now = new Date(timestamp);
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
 // API Integration
 function displayData(response) {
   console.log(response);
@@ -36,6 +51,8 @@ function displayData(response) {
   let minimumElement = document.querySelector("#minimum");
   let maximumElement = document.querySelector("#maximum");
   let dateElement = document.querySelector("#date");
+  let sunriseElement = document.querySelector("#sunrise");
+  let sunsetElement = document.querySelector("#sunset");
 
   temperatureELement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -45,9 +62,11 @@ function displayData(response) {
   minimumElement.innerHTML = Math.round(response.data.main.temp_min);
   maximumElement.innerHTML = Math.round(response.data.main.temp_max);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  sunriseElement.innerHTML = formatSun(response.data.sys.sunrise * 1000);
+  sunsetElement.innerHTML = formatSun(response.data.sys.sunset * 1000);
 }
 
 let apiKey = "da4354ccc4b5c937168c50391a787c99";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Cologne&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayData);
