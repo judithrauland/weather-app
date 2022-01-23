669e5ec437c68ad7081e055000cb3742454b1ed8
judithrauland <97590487+judithrauland@users.noreply.github.com>
2022-01-23 12:36:33 +0100

    Updated CSS, removed sunrise/sunset data

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 5) {
+    if (index < 6) {
       forecastHTML =
         forecastHTML +
         `
@@ -72,26 +72,10 @@ function displayForecast(response) {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
-}
-// Time for sunrise and sunset
-function formatSun(timestamp) {
-  let now = new Date(timestamp);
-  let hours = now.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = now.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  return `${hours}:${minutes}`;
 }
 
 // Coordinates of Forecast City
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "da4354ccc4b5c937168c50391a787c99";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
@@ -107,8 +91,6 @@ function displayData(response) {
   let minimumElement = document.querySelector("#minimum");
   let maximumElement = document.querySelector("#maximum");
   let dateElement = document.querySelector("#date");
-  let sunriseElement = document.querySelector("#sunrise");
-  let sunsetElement = document.querySelector("#sunset");
   let iconElement = document.querySelector("#icon");
 
   celsiusTemperature = response.data.main.temp;
@@ -121,8 +103,6 @@ function displayData(response) {
   minimumElement.innerHTML = Math.round(response.data.main.temp_min);
   maximumElement.innerHTML = Math.round(response.data.main.temp_max);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  sunriseElement.innerHTML = formatSun(response.data.sys.sunrise * 1000);
-  sunsetElement.innerHTML = formatSun(response.data.sys.sunset * 1000);
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
